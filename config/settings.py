4275b5023c042fa96effecf4a87426506b460165
Sina Baniasad Azad <sina.baniasadazad@gmail.com>
2026-08-11 16:20:41 +0330

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 00110011
 import os
 from pathlib import Path
-0101010101
+01010101010
 from dotenv import load_dotenv
 
 PROJECT_ROOT = Path(__file__).resolve().parent.parent
