dd43ed1acec0c39c02bf2343e56e805db2d92c91
Sina Baniasad Azad <sina.baniasadazad@gmail.com>
2026-08-15 13:17:59 +0330

    Update app.js

--- web/app.js
+++ web/app.js
@@ -5,7 +5,7 @@
    ============================================================ */
 (() => {
   'use strict';
-0
+00
   // ---------- Label metadata ----------
   const LAYER_CAPTIONS = { layer1: 'Type', layer2: 'Domain' };
   const LABELS = {
